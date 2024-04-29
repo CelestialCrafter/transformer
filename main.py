288916ce90c21da8a4ea6bb6159547a2b935227c
CelestialCrafter <celestialcraftermc@gmail.com>
2024-04-29 15:07:35 -0500

    remove dummy things + notebook things

--- main.py
+++ main.py
@@ -10,49 +10,11 @@ from torch.optim.lr_scheduler import LambdaLR
 import pandas as pd
 import altair as alt
 from torch.utils.data import DataLoader
-import warnings
 from torch.utils.data.distributed import DistributedSampler
 import torch.distributed as dist
 import torch.multiprocessing as mp
 from torch.nn.parallel import DistributedDataParallel as DDP
 
-
-# Set to False to skip notebook execution (e.g. for debugging)
-warnings.filterwarnings("ignore")
-RUN_EXAMPLES = True
-
-# Some convenience helper functions used throughout the notebook
-
-
-def is_interactive_notebook():
-    return __name__ == "__main__"
-
-
-def show_example(fn, args=[]):
-    if __name__ == "__main__" and RUN_EXAMPLES:
-        return fn(*args)
-
-
-def execute_example(fn, args=[]):
-    if __name__ == "__main__" and RUN_EXAMPLES:
-        fn(*args)
-
-
-class DummyOptimizer(torch.optim.Optimizer):
-    def __init__(self):
-        self.param_groups = [{"lr": 0}]
-        None
-
-    def step(self):
-        None
-
-    def zero_grad(self, set_to_none=False):
-        None
-
-
-class DummyScheduler:
-    def step(self):
-        None
 
 class EncoderDecoder(nn.Module):
     """
